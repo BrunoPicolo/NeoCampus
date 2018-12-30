d1035827a1b672e85d737a9d88cce80780bcd9ed
BrunoPicolo <picolobruno@hotmail.com>
2018-12-30 10:58:23 +0100

    Classe ManagerIHM et Graphe ajoté. Seulement implementation de quelque méthode

---
+++ project/src/donnees/Graphe.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package donnees;
+
+import java.util.List;
+
+/**
+ * @author bruno
+ *
+ */
+public class Graphe {
+	
+	public void afficher(Capteur capteur, List<Double> donnees) {
+		//TODO ...
+	}
+	
+	public void afficher(List<Double> mesuresCapteur) {
+		//TODO ...
+	}
+	
+	public void nettoyer() {
+		//TODO ...
+	}
+	
+}
