30f572772b30b1995b87a8acb8884a1003bb852a
BrunoPicolo <picolobruno@hotmail.com>
2018-12-30 10:39:32 +0100

    Quelque avance sur le managerDonnes

--- project/src/donnees/ManagerDonnees.java
+++ project/src/donnees/ManagerDonnees.java
@@ -3,6 +3,9 @@
  */
 package donnees;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.NavigableSet;
 import java.util.TreeSet;
 
@@ -29,4 +32,52 @@ public class ManagerDonnees {
 		this.motDePasse = motDePasse;
 		this.capteurConnectes = new TreeSet<>();
 	}
+	
+	/**
+	 * 
+	 * @param nomCapteur
+	 * @param valeur
+	 */
+	public void actualiserValeurCapteur(String nomCapteur, double valeur) {
+		for(Capteur capteur : capteurConnectes) {
+			if (capteur.getNom().equals(nomCapteur)) {
+				capteur.actualiserValeur(valeur);
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param nom
+	 * @param batiment
+	 * @param lieu
+	 * @param etage
+	 * @param type
+	 */
+	public void connecterCapteur(String nom, String batiment, String lieu, int etage, TypeCapteur type) {
+		Capteur capteur = new Capteur(nom, batiment, lieu, etage, type);
+		capteurConnectes.add(capteur);
+	}
+	
+	public List<Double> mesuresPeriode(Capteur capteur, Date dateMin, Date DateMax) {
+		List<Double> mesures = new ArrayList<>();
+		//TODO demander a capteursTableModel les valeurs pendant la periode
+		return mesures;
+	}
+	
+	public List<TypeCapteur> getCapteursBD(TypeCapteur type){
+		List<TypeCapteur> capteurs = new ArrayList<>();
+		//TODO meme chose que methode precedente
+		return capteurs;
+	}
+	
+	public List<TypeCapteur> getCapteursConnectes(){
+		List<TypeCapteur> capteurs = new ArrayList<>();
+		//TODO meme chose que methode precedente
+		return capteurs;
+	}
+	
+	public void deconnecterCapteur(Capteur capteur) {
+		
+	}
+	
 }
