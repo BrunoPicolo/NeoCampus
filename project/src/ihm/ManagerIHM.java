71067a34d569d1a37f8e768502806fa03be8ac72
BrunoPicolo <picolobruno@hotmail.com>
2018-12-30 21:44:55 +0100

    Implémentation en motie de la fenêtre principale

--- project/src/ihm/ManagerIHM.java
+++ project/src/ihm/ManagerIHM.java
@@ -3,9 +3,14 @@
  */
 package ihm;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
+import javax.swing.JPanel;
 import donnees.ManagerDonnees;
 
 /**
@@ -17,6 +22,10 @@ public class ManagerIHM {
 	private ManagerDonnees managerDonnees;
 	private static int port;
 	
+	/**
+	 * 
+	 * @param managerDonnees
+	 */
 	public ManagerIHM(ManagerDonnees managerDonnees) {
 		this.managerDonnees = managerDonnees;
 	}
@@ -26,7 +35,41 @@ public class ManagerIHM {
 		JFrame connexion = new JFrame("Connexion");
 		String strPort = JOptionPane.showInputDialog(connexion,
 				"Numéro de port:", DEFAULT_PORT);
-		port = Integer.parseInt(strPort);
+		if (strPort == null) 
+			port = DEFAULT_PORT;
+		else
+			port = Integer.parseInt(strPort);
+		connexion.dispose();
+		fenetrePrincipale();
+	}
+	
+	private static void fenetrePrincipale() {
+		JFrame frame = new JFrame("NeoCampus");
+		JPanel base = new JPanel();
+		JPanel arborescenceCapteurs = new JPanel();
+		JPanel analyseurDonnees = new JPanel();
+		JPanel analyseTempsReel = new JPanel();
+		BorderLayout borderlayout = new BorderLayout();
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension currentScreenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
+		frame.setSize(currentScreenSize);
+		
+		arborescenceCapteurs.add(new JButton("touch me"));
+		analyseTempsReel.add(new JButton("suck me"));
+		analyseurDonnees.add(new JButton("love me"));
+		
+		base.setLayout(borderlayout);
+		base.add(arborescenceCapteurs, BorderLayout.LINE_START);
+		base.add(analyseurDonnees,BorderLayout.LINE_END);
+		base.add(analyseTempsReel,BorderLayout.CENTER);
+		
+		
+		frame.getContentPane().add(base);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 	
 	/**
