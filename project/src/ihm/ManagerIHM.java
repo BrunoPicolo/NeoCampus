1d0e0630891f14785c7ea211af69f9a61bca0439
BrunoPicolo <picolobruno@hotmail.com>
2018-12-31 13:52:44 +0100

    Structure de la fenetre principale

--- project/src/ihm/ManagerIHM.java
+++ project/src/ihm/ManagerIHM.java
@@ -5,12 +5,17 @@ package ihm;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import donnees.ManagerDonnees;
 
 /**
@@ -21,6 +26,9 @@ public class ManagerIHM {
 	private static final int DEFAULT_PORT = 8952;
 	private ManagerDonnees managerDonnees;
 	private static int port;
+	private static Object[][] donneesCapteurs;
+	private int nbCapteurs = 0;
+	private static final String[] entete = {"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"};
 	
 	/**
 	 * 
@@ -40,36 +48,50 @@ public class ManagerIHM {
 		else
 			port = Integer.parseInt(strPort);
 		connexion.dispose();
-		fenetrePrincipale();
 	}
 	
 	private static void fenetrePrincipale() {
+//		Composant principaux
 		JFrame frame = new JFrame("NeoCampus");
-		JPanel base = new JPanel();
-		JPanel arborescenceCapteurs = new JPanel();
-		JPanel analyseurDonnees = new JPanel();
-		JPanel analyseTempsReel = new JPanel();
-		BorderLayout borderlayout = new BorderLayout();
+		JPanel base = new JPanel(new BorderLayout());
+		JPanel analysePanel = new JPanel(new BorderLayout());
 		
+		JPanel arborescenceCapteurs = new JPanel(new BorderLayout());
+		JPanel analyseurDonnees = new JPanel(new BorderLayout());
+		JPanel analyseTempsReel = new JPanel(new BorderLayout());
 		
+		base.add(arborescenceCapteurs, BorderLayout.LINE_START);
+		base.add(analysePanel,BorderLayout.LINE_END);
+	
+		analysePanel.add(analyseurDonnees,BorderLayout.PAGE_START);
+		analysePanel.add(analyseTempsReel,BorderLayout.PAGE_END);
+		
+//		analyseTempsReel arrangement des composants 
+		JLabel titreATR = new JLabel("Analyseur Temps Réel");
+		JTable tableau = new JTable(donneesCapteurs,entete);
+		
+		analyseTempsReel.add(titreATR, BorderLayout.PAGE_START);
+		analyseTempsReel.add(tableau.getTableHeader(),BorderLayout.CENTER);
+		//analyseTempsReel.add(tableau,BorderLayout.CENTER); //Pour afficher les données, Erreur si 0 capteurs
+		
+//		analyseur De Données arrangement des composants
+		JLabel titreAD = new JLabel("Analyseur De Données");
+		analyseurDonnees.add(titreAD,BorderLayout.PAGE_START);
+		
+//		arborescence  de capteurs arrangement des composants
+		JLabel titreAC = new JLabel("Arborescence Capteurs");
+		arborescenceCapteurs.add(titreAC,BorderLayout.PAGE_START);
+		
+//		JFrame parametrage
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension currentScreenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
 		frame.setSize(currentScreenSize);
-		
-		arborescenceCapteurs.add(new JButton("touch me"));
-		analyseTempsReel.add(new JButton("suck me"));
-		analyseurDonnees.add(new JButton("love me"));
-		
-		base.setLayout(borderlayout);
-		base.add(arborescenceCapteurs, BorderLayout.LINE_START);
-		base.add(analyseurDonnees,BorderLayout.LINE_END);
-		base.add(analyseTempsReel,BorderLayout.CENTER);
-		
-		
 		frame.getContentPane().add(base);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
+		frame.pack(); // à enlever après les tests!
 		frame.setVisible(true);
+		
 	}
 	
 	/**
@@ -79,6 +101,7 @@ public class ManagerIHM {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				fenetreDeConnexion();
+				fenetrePrincipale();
 			}
 		});
 	}
