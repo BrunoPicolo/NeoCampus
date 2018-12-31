fdb7b7029b78b8b4c15bc5f1d54d891c4d63af28
BrunoPicolo <picolobruno@hotmail.com>
2018-12-31 18:52:30 +0100

    Amelioration de la fenetre principale

--- project/src/ihm/ManagerIHM.java
+++ project/src/ihm/ManagerIHM.java
@@ -8,12 +8,14 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import donnees.ManagerDonnees;
@@ -26,7 +28,12 @@ public class ManagerIHM {
 	private static final int DEFAULT_PORT = 8952;
 	private ManagerDonnees managerDonnees;
 	private static int port;
-	private static Object[][] donneesCapteurs;
+	private static Object[][] donneesCapteurs = {
+			{"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"},
+			{"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"},
+			{"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"},
+			{"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"}
+		    };
 	private int nbCapteurs = 0;
 	private static final String[] entete = {"Capteur","Fluide","Batiment","Etage","Lieu","Seuil min","Seuil max"};
 	
@@ -61,17 +68,19 @@ public class ManagerIHM {
 		JPanel analyseTempsReel = new JPanel(new BorderLayout());
 		
 		base.add(arborescenceCapteurs, BorderLayout.LINE_START);
-		base.add(analysePanel,BorderLayout.LINE_END);
+		
+		base.add(analysePanel,BorderLayout.CENTER);
 	
 		analysePanel.add(analyseurDonnees,BorderLayout.PAGE_START);
 		analysePanel.add(analyseTempsReel,BorderLayout.PAGE_END);
+		
 		
 //		analyseTempsReel arrangement des composants 
 		JLabel titreATR = new JLabel("Analyseur Temps Réel");
 		JTable tableau = new JTable(donneesCapteurs,entete);
 		
 		analyseTempsReel.add(titreATR, BorderLayout.PAGE_START);
-		analyseTempsReel.add(tableau.getTableHeader(),BorderLayout.CENTER);
+		analyseTempsReel.add(new JScrollPane(tableau),BorderLayout.CENTER); 
 		//analyseTempsReel.add(tableau,BorderLayout.CENTER); //Pour afficher les données, Erreur si 0 capteurs
 		
 //		analyseur De Données arrangement des composants
@@ -87,10 +96,9 @@ public class ManagerIHM {
 		Dimension currentScreenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
 		frame.setSize(currentScreenSize);
 		frame.getContentPane().add(base);
-		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
-		frame.pack(); // à enlever après les tests!
+		frame.pack(); 
 		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 		
 	}
 	
@@ -100,7 +108,7 @@ public class ManagerIHM {
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				fenetreDeConnexion();
+//				fenetreDeConnexion();
 				fenetrePrincipale();
 			}
 		});
